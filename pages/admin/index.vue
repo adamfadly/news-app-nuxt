9e13d40cfdd6e22aafbe69bd89ae017278c8a652
Adam Fadly <adamfadly11@gmail.com>
2021-08-30 21:58:01 +0700

    set token store in state and post news with auth

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -7,6 +7,7 @@
 <script>
 import AdminMain from '../../components/views/admin/AdminMain.vue'
 import axios from 'axios'
+import { mapState, mapMutations } from 'vuex'
 
 export default {
   components: {
@@ -18,14 +19,18 @@ export default {
       news: {}
     }
   },
+  computed: {
+    ...mapState({ token: 'token' })
+  },
   methods: {
+    ...mapMutations(['addNewPost']),
     submitPost(postData) {
       axios
         .post(
-          'https://news-app-nuxt-80b8e-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json',
+          `https://news-app-nuxt-80b8e-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json?auth=${this.token}`,
           postData
         )
-        .then(this.$router.push('/'))
+        .then(this.addNewPost(postData), this.$router.push('/'))
         .catch(error => console.log(error))
     }
   }
--- pages/index.vue
+++ pages/index.vue
@@ -14,6 +14,7 @@ import Card from '../components/News/Card.vue'
 export default {
   components: { Card },
 
+  methods: {},
   computed: {
     articles() {
       return this.$store.state.news
